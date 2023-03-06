8f1209d1aac4ee53e2ee095be45c82e3140bb1dd
Farhan <farhan@Farhans-MacBook-Air.local>
2023-03-06 22:04:28 +0600

    focus btn modified

--- js/instructorPortal.js
+++ js/instructorPortal.js
@@ -1,57 +1,83 @@
 const iPortal = () => {
-    displayButtons(openModal + pressE + focus + submitMark + assimentAdd+reload + closeModal);
+	displayButtons(
+		openModal +
+			pressE +
+			focus +
+			submitMark +
+			assimentAdd +
+			closeModal +
+			reload
+	);
 
+	const focusButton = getElement(true, "focus");
+	const modalOpen = getElement(true, "openModal");
+	const modalClose = getElement(true, "closeModal");
+	const press = getElement(true, "pressE");
+	const submitMarkSecondary = getElement(true, "submitMark");
+	const addAssignment = getElement(true, "addAssignment");
+	const fullMarkBtn = getElement(true, "fullMark");
 
-    const focusButton = getElement(true, 'focus');
-    const modalOpen = getElement(true, 'openModal');
-    const modalClose = getElement(true, 'closeModal');
-    const press = getElement(true, 'pressE');
-    const submitMarkSecondary = getElement(true, 'submitMark');
-    const addAssignment = getElement(true, 'addAssignment');
+	focusButton.addEventListener("click", function () {
+		const inputMark = getElement(true, "Mark");
+		const insertBtn = getElement(true, "insertBtn");
+		insertBtn.click();
+		const suggetMark = document
+			.getElementsByClassName("m-2 w-50 markSuggestions")[0]
+			.innerText.split(" ")[0];
+		navigator.clipboard.writeText(parseInt(suggetMark));
+		inputMark.focus();
+	});
 
+	//click to submit mark
+	submitMarkSecondary.addEventListener("click", function () {
+		const submitButtonPrimary = getElement(
+			false,
+			"btn px-4 btn-primary"
+		)[0];
+		submitButtonPrimary.click();
+	});
 
-    focusButton.addEventListener('click', function () {
-        const inputMark = getElement(true, 'Mark');
-        inputMark.focus(() => {
-            inputMark.value = 22;
-            document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 3 }));
+	//open 1st assignment
+	modalOpen.addEventListener("click", function () {
+		const open = getElement(
+			false,
+			"btn btn-icon btn-eye-icon btn-primary "
+		);
+		open[0].click();
+	});
 
-        });
-        document.dispatchEvent(new KeyboardEvent('keydown', { 'key': 3 }));
-    });
+	//close the Modal window
+	modalClose.addEventListener("click", function () {
+		const close = getElement(false, "btn btn-close");
+		close[0].click();
+	});
 
-    //click to submit mark
-    submitMarkSecondary.addEventListener('click', function () {
-        const submitButtonPrimary = getElement(false, 'btn px-4 btn-primary')[0];
-        submitButtonPrimary.click();
-    });
+	//press E
+	press.addEventListener("click", function () {
+		document.dispatchEvent(new KeyboardEvent("keydown", { key: "]" }));
+	});
 
-    //open 1st assignment
-    modalOpen.addEventListener('click', function () {
-        const open = getElement(false, 'btn btn-icon btn-eye-icon btn-primary ');
-        open[0].click();
-    });
+	//add assignment
+	addAssignment.addEventListener("click", function () {
+		const checkbox = getElement(true, "thead-checkbox");
+		const assignBtn = getElement(
+			false,
+			"low-op-btn float-right btn btn-outline-primary"
+		)[0];
+		checkbox.click();
+		assignBtn.click();
+		const okBtn = getElement(
+			false,
+			"swal-button swal-button--confirm swal-button--danger"
+		)[0];
 
-    //close the Modal window
-    modalClose.addEventListener('click', function () {
-        const close = getElement(false, 'btn btn-close');
-        close[0].click();
-    });
+		okBtn.click();
+	});
 
-    //press E
-    press.addEventListener('click', function () {
-        document.dispatchEvent(new KeyboardEvent('keydown', { 'key': ']' }));
-    });
-
-    //add assignment
-    addAssignment.addEventListener('click', function () { 
-        const checkbox = getElement(true, 'thead-checkbox');
-        const assignBtn = getElement(false, 'low-op-btn float-right btn btn-outline-primary')[0];
-        checkbox.click();
-        assignBtn.click();
-        const okBtn = getElement(false, 'swal-button swal-button--confirm swal-button--danger')[0];
-okBtn.click();
-    })
-
-
-}
+	// //give full mark
+	// fullMarkBtn.addEventListener("click", function () {
+	// 	navigator.clipboard.writeText(60);
+	// 	// press.click();
+	// 	focusButton.click();
+	// });
+};
