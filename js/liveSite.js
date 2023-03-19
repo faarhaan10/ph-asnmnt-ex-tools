7e5756775dc509a7d198b4d047f44e861583310b
Shakil Ahmed Atik <shakilatik@gmail.com>
2023-03-19 22:38:10 +0600

    close-tab button added

--- js/liveSite.js
+++ js/liveSite.js
@@ -1,36 +1,33 @@
 const liveSite = () => {
-	displayButtons(scrollTop + reload + scrollBottom);
+  displayButtons(scrollTop + reload + scrollBottom + closeTab)
 
-	const top = getElement(true, "scrollTop");
-	const down = getElement(true, "scrollBottom");
-	const action = getElement(true, "actionBtn");
-	const help = getElement(
-		false,
-		"bg-orange-600 px-20 py-2 text-white rounded fixed top-2 left-[45%]"
-	)[0];
-	help.click()
+  const top = getElement(true, 'scrollTop')
+  const down = getElement(true, 'scrollBottom')
+  const action = getElement(true, 'actionBtn')
+  const help = getElement(
+    false,
+    'bg-orange-600 px-20 py-2 text-white rounded fixed top-2 left-[45%]'
+  )[0]
+  help.click()
 
-	top.addEventListener("click", function () {
-		window.scrollTo(0, 0);
-	});
+  top.addEventListener('click', function () {
+    window.scrollTo(0, 0)
+  })
 
-	down.addEventListener("click", function () {
-		window.scrollTo(0, 9999999999);
-	});
-	action.addEventListener("click", function () {
-		
-		const restart = getElement(
-			false,
-			"bg-green-600 text-white w-full py-2 rounded mt-16"
-		)[0];
-		 
-		if (restart) {
-			restart.click();
-			help.click();
-		}
-		else {
-			help.click();
-		}
+  down.addEventListener('click', function () {
+    window.scrollTo(0, 9999999999)
+  })
+  action.addEventListener('click', function () {
+    const restart = getElement(
+      false,
+      'bg-green-600 text-white w-full py-2 rounded mt-16'
+    )[0]
 
-	});
-};
+    if (restart) {
+      restart.click()
+      help.click()
+    } else {
+      help.click()
+    }
+  })
+}
