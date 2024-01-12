ab49c63a79b9836185b29b6d834991f69454f15b
Farhan Elias <faarhaan10@gmail.com>
2024-01-12 23:53:24 +0600

    git view btn updtd

--- js/github.js
+++ js/github.js
@@ -3,15 +3,21 @@ const github = () => {
 
   const gitAction = () => {
     const date =
-      document.getElementsByTagName("relative-time")[0].attributes.title.value;
-    const commits = document.getElementsByTagName("strong")[13].innerHTML;
+      document.getElementsByTagName("relative-time")[0].attributes.title
+        .value || "N/A";
+    const commitsElmnt = document.getElementsByClassName(
+      "Text-sc-17v1xeu-0 hfRvxg"
+    );
+
+    // update: commits location
+    const commits = commitsElmnt[0]?.innerHTML || "N/A";
+    // document.getElementsByTagName("strong")[13].innerHTML || "N/A";
 
     const resultBtn = `<button id="handleCopy" type="button" class="btn btn-primary text-bolder mb-2 ">
       <span>${date}</span>
       <hr class='m-0'/>
-      <span>Total Commits:- ${commits}</span>
+      <span>Total: ${commits}</span>
       </button>`;
-
     displayButtons(resultBtn + closeTab + reload + view);
 
     //to copy
@@ -22,6 +28,7 @@ const github = () => {
       });
   };
   document.getElementById("handleView").addEventListener("click", function () {
+    console.log("object", 11);
     gitAction();
   });
 
